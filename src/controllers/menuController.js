79ade227d535832ecb8ac94b23cd090221036abc
Shawn Smith <sds.smith24@gmail.com>
2024-10-05 08:46:09 -0400

    move helper functions and develop env vars

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -1,38 +1,19 @@
-const buildCategoriesObj = (categoriesArr) => 
-    categoriesArr.reduce((acc, curr) => {
-        if (!acc[curr.name.toLowerCase()]) {
-            return {
-              ...acc,
-              [curr.name.toLowerCase()]: {
-                ...curr,
-                items: []
-              }
-            }
-        } else {
-            return acc
-        }
-    }, {})
+import dotenv from 'dotenv';
+dotenv.config();
 
-const buildMenuItemsByCategory = (items, categories) => 
-    items.reduce((acc, curr) => ({
-        ...acc,
-        [curr.category?.toLowerCase()]: {
-          ...acc[curr.category.toLowerCase()], 
-          items: [...acc[curr.category.toLowerCase()].items, curr]
-        }
-      }), categories)
+import { 
+    buildCategoriesObj, 
+    buildMenuItemsByCategory, 
+    buildMenuItemsById 
+} from "../helpers/menuHelpers.js";
 
-const buildMenuItemsById = (items) =>
-    items.reduce((acc, curr) => ({
-        ...acc,
-        [curr.id]: curr
-      }), {})
+const MENU_API = process.env.MENU_DATA_API_BASE_URL
 
 export async function getMenuData() {
-    const categoriesResponse = await fetch('http://localhost:3000/api/v1/categories')
+    const categoriesResponse = await fetch(`${MENU_API}/categories`);
     const categoriesData = await categoriesResponse.json();
     const categories = buildCategoriesObj(categoriesData);
-    const itemsResponse = await fetch('http://localhost:3000/api/v1/items');
+    const itemsResponse = await fetch(`${MENU_API}/items`);
     const items = await itemsResponse.json();
     const menuItemsByCategory = buildMenuItemsByCategory(items, categories);
     const menuItemsById = buildMenuItemsById(items);
---
+++ src/helpers/menuHelpers.js
@@ -0,0 +1,29 @@
+export const buildCategoriesObj = (categoriesArr) => 
+    categoriesArr.reduce((acc, curr) => {
+        if (!acc[curr.name.toLowerCase()]) {
+            return {
+              ...acc,
+              [curr.name.toLowerCase()]: {
+                ...curr,
+                items: []
+              }
+            }
+        } else {
+            return acc
+        }
+    }, {})
+
+export const buildMenuItemsByCategory = (items, categories) => 
+    items.reduce((acc, curr) => ({
+        ...acc,
+        [curr.category?.toLowerCase()]: {
+          ...acc[curr.category.toLowerCase()], 
+          items: [...acc[curr.category.toLowerCase()].items, curr]
+        }
+      }), categories)
+
+export const buildMenuItemsById = (items) =>
+    items.reduce((acc, curr) => ({
+        ...acc,
+        [curr.id]: curr
+      }), {})
