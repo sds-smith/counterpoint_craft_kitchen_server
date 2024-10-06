08b59d33c1808ac663047973c55ac1cd570a6c77
Shawn Smith <sds.smith24@gmail.com>
2024-10-06 09:25:14 -0400

    cleanup

--- src/routes/auth/paypal/paypalAuthRouter.js
+++ src/routes/auth/paypal/paypalAuthRouter.js
@@ -3,15 +3,6 @@ import { createOrder, captureOrder } from '../../../helpers/paypalHelpers.js';
 
 const paypalAuthRouter = Router();
 
-// paypalAuthRouter.get('/', async (_req, res) => {
-//     try {
-//         const orderResponse = await createOrder('carty cart');
-//         res.status(200).json({message: `hello from paypalAuthRouter`, orderResponse})
-//     } catch(err) {
-//         res.status(500).json(err)
-//     }
-// })
-
 paypalAuthRouter.post("/orders", async (req, res) => {
     try {
       // use the cart information passed from the front-end to calculate the order amount detals
